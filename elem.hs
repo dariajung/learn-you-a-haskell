9cc662d75eb9797698bad8716518eac300015989
Daria Jung <djj2115cu@gmail.com>
2014-04-20 21:08:37 -0400

    use foldr to refactor map

--- elem.hs
+++ elem.hs
@@ -1,5 +1,6 @@
 elem' :: (Eq a) => a -> [a] => Bool
-elem' a [] = False
-elem' a (x:xs)
-    | a == x    = True
-    | otherwise = elem' a xs
+-- elem' a [] = False
+-- elem' a (x:xs)
+--    | a == x    = True
+--    | otherwise = elem' a xs
+elem' y ys  = foldl (\acc x -> if x == y then True else acc) False ys
--- map.hs
+++ map.hs
@@ -1,3 +1,4 @@
 map' :: (a -> b) -> [a] -> [b]
-map' _ []   = []
-map' f (x:xs) = f x : map' f xs
+-- map' _ []   = []
+-- map' f (x:xs) = f x : map' f xs
+map' f xs = foldr (\x acc -> f x : acc) [] xs
