e9b77ba26af81e8cba73ca6c637cb1a42a4659b0
Daria Jung <djj2115cu@gmail.com>
2014-04-26 19:25:21 -0400

    implement findKey using right fold

--- findKey.hs
+++ findKey.hs
@@ -1,7 +1,9 @@
 import qualified Data.Map as Map
 
 findKey :: (Eq k) => k -> [(k, v)] -> Maybe v
-findKey key [] = Nothing
-findKey key ((k,v):xs) = if k == key
-                            then Just v
-                            else findKey key xs
+-- findKey key [] = Nothing
+-- findKey key ((k,v):xs) = if k == key
+--                            then Just v
+--                            else findKey key xs
+
+findKey key = foldr (\(k, v) acc -> if k == key then Just v else acc) Nothing
